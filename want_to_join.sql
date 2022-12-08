7d2244bcd466b273153af883089aeb050f4a4cec
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-08 22:35:59 +0500

    MySQL

---
+++ want_to_join.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE want_to_join;
+USE want_to_join;
+CREATE TABLE personalities_info(
+	names VARCHAR(30),
+	reg_no VARCHAR(12),
+    degree VARCHAR(20),
+    address VARCHAR(50)
+    );
+INSERT INTO personalities_info
+VALUES
+	('MUDASIR','2021-ag-6449','ARE','Mian Channu'),
+	('ZAHOOR','2021-ag-7843','BSCS','Faisalabad'),
+    ('FAIZAN','2021-ag-7776','BSCS','Khanewal'),
+	('ABDULLAH','2021-ag-7779','BSCS','Faisalabad'),
+     ('ARHAM','2021-ag-7760','BSCS','Faisalabad');
