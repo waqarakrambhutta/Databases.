8d8170b8c4348cfc80deddf5dcee2359992ec617
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-13 22:38:09 +0500

    MySQL

--- friends.sql
+++ friends.sql
@@ -4,4 +4,3 @@ CREATE TABLE A_cluster(name_of_friends VARCHAR(50),REG_number varchar(12));
 INSERT INTO A_cluster VALUES('Faizan Yousaf','2021-ag-7776');
 INSERT INTO A_cluster VALUES('Zahoor Ahmed','2021-ag-7754');
 INSERT INTO A_cluster VALUES('Waqar Akram','2021-ag-7843');
-A_cluster
