4abcc7c6d63c4082db295ee785cdc1fbdafc4d3f
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-13 22:36:26 +0500

    MySQL

--- DATABASES.sql
+++ DATABASES.sql
@@ -1,4 +1,8 @@
 CREATE DATABASE friends;
 USE friends;
 CREATE TABLE A_cluster(name_of_friends VARCHAR(50),REG_number varchar(12));
-
+insert into A_cluster(name_of_friends, REG_number)
+values
+('faizan','2021-ag-7776'),
+('zahoor','2021-ag-7754'),
+('waqar','2021-ag-7843');
